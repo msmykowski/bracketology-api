441c5c426e0c75ce4666c573ff4564c46c55db4f
msmykowski <smycal@gmail.com>
2015-02-25 10:57:24 -0800

    seeded DB with players and teams

--- db/migrate/20150225050613_create_teams.rb
+++ db/migrate/20150225050613_create_teams.rb
@@ -2,6 +2,23 @@ class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
       t.string :name
+      t.string :img_url
+      t.string :o_ppg
+      t.string :o_rpg
+      t.string :o_apg
+      t.string :o_fg
+      t.string :d_ppg
+      t.string :d_rpg
+      t.string :d_bpg
+      t.string :d_spg
+      t.string :o_ppg_rank
+      t.string :o_rpg_rank
+      t.string :o_apg_rank
+      t.string :o_fg_rank
+      t.string :d_ppg_rank
+      t.string :d_rpg_rank
+      t.string :d_bpg_rank
+      t.string :d_spg_rank
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,23 @@ ActiveRecord::Schema.define(version: 20150225050613) do
 
   create_table "teams", force: true do |t|
     t.string   "name"
+    t.string   "img_url"
+    t.string   "o_ppg"
+    t.string   "o_rpg"
+    t.string   "o_apg"
+    t.string   "o_fg"
+    t.string   "d_ppg"
+    t.string   "d_rpg"
+    t.string   "d_bpg"
+    t.string   "d_spg"
+    t.string   "o_ppg_rank"
+    t.string   "o_rpg_rank"
+    t.string   "o_apg_rank"
+    t.string   "o_fg_rank"
+    t.string   "d_ppg_rank"
+    t.string   "d_rpg_rank"
+    t.string   "d_bpg_rank"
+    t.string   "d_spg_rank"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
